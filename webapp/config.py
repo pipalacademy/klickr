98874181edc0a40f047e8a9fa8e1b144de8b082f
Nabarun Pal <pal.nabarun95@gmail.com>
2019-05-11 23:48:03 +0530

    Adds s3 related env vars to config

--- webapp/config.py
+++ webapp/config.py
@@ -1,2 +1,10 @@
+import os
+
 DATABASE_URL = os.getenv('DATABASE_URL', 'postgres:///klickr')
 REDIS_URL = os.getenv('REDIS_URL', 'redis://')
+
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", "us-east-1")
+AWS_ENDPOINT_URL = os.getenv("AWS_ENDPOINT_URL")
+S3_BUCKET = os.getenv("S3_BUCKET", "klickr-pal-test")
